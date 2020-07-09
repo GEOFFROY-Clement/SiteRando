48faa829de4e5d407fbda92e71e7cd035b783d7d
GEOFFROY-Clement <clement.geoffroy@gmail.com>
2020-07-09 14:15:53 +0200

    add line on DataBase

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,28 +24,17 @@ namespace SiteRando.Controllers
 
         public IActionResult Index()
         {
-            List<Hike> randos = new List<Hike>();
-            randos.Add(new Hike("test", 10, 50, 20, 3, "totot", "tata", "je fais ce que je veux", "Marseille"));
-            randos.Add(new Hike("Rando préférée", 1000, 280, 120, 2, "nous commençons sur la place de la mairie", "nous finissons au bar du coin", "a boire a boire a boire", "Thonon les bains"));
-            randos.Add(new Hike( "Rando que je deteste", 1000, 280, 120, 2, "nous commençons sur la place de la mairie", "nous finissons au bar du coin", "a boire a boire a boire", "Thonon les bains"));
-            ViewBag.randoLast = randos;
 
-            List<Hike> randosFav = new List<Hike>();
-            randosFav.Add(new Hike( "Je suis ", 10, 50, 20, 3, "totot", "tata", "je fais ce que je veux", "Marseille"));
-            randosFav.Add(new Hike( "Un excellent", 1000, 280, 120, 2, "nous commençons sur la place de la mairie", "nous finissons au bar du coin", "a boire a boire a boire", "Thonon les bains"));
-            randosFav.Add(new Hike( "Developpeur", 1000, 280, 120, 2, "nous commençons sur la place de la mairie", "nous finissons au bar du coin", "a boire a boire a boire", "Thonon les bains"));
-            ViewBag.randoFav = randosFav;
 
             DbSet<Hike> hikes = _context.Hikes;
             ViewBag.hikes = hikes;
-
             return View();
         }
 
         public IActionResult Privacy()
         {
-            
-            //Hike hike = new Hike( "test", 10, 50, 20, 3, "totot", "tata", "je fais ce que je veux", "Marseille");
+
+            //Hike hike = new Hike("test", 10, 50, 20, 3, "totot", "tata", "je fais ce que je veux", "Marseille");
             //_context.Add(hike);
             //_context.SaveChanges();
             return View();
